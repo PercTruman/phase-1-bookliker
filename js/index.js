562b0798ceae458e8a5b75c50623fca089c6cdc3
PercTruman <drshawnhart@gmail.com>
2022-05-11 20:41:19 -0500

    got the list of titles to display onscreen

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('http://localhost:3000/books')
+    .then(res=>res.json())
+    .then(bookObjects=>bookObjects.forEach(book=>renderBook(book)))
+});
+
+function renderBook(book){
+    let unorderedList= document.getElementById('list')
+    let titleText= book.title
+    let listItem = document.createElement('li')
+    listItem.innerText=titleText
+    unorderedList.appendChild(listItem)
+
+    listItem.addEventListener('click', (e)=>displayBookInfo(e))
+}
+
+function displayBookInfo(e){
+    console.log(e)
+}
